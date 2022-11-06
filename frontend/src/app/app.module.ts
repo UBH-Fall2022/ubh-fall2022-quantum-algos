0b0c7efa78f083824dee6c0c3fc56e314a1c3ed7
m1lkman896 <michaelhorvath896@gmail.com>
2022-11-06 02:21:09 -0500

    🚑 FrontEnd HTML

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppHomepageComponent } from './app-homepage/app-homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    AppHomepageComponent
   ],
   imports: [
     BrowserModule,
